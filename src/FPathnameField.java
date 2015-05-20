ccc137cf3da67627f5d98998c47a0f98eae38d5a
Andy Morgan-Richards <id883436@prng.eu>
2015-05-20 07:30:54 +0100

    Initial version.

---
+++ src/FPathnameField.java
@@ -0,0 +1,121 @@
+/*====================================================================*\
+
+FPathnameField.java
+
+Pathname field class.
+
+\*====================================================================*/
+
+
+// IMPORTS
+
+
+import java.io.File;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import uk.org.blankaspect.gui.GuiUtilities;
+
+import uk.org.blankaspect.textfield.PathnameField;
+
+//----------------------------------------------------------------------
+
+
+// PATHNAME FIELD CLASS
+
+
+class FPathnameField
+    extends PathnameField
+{
+
+////////////////////////////////////////////////////////////////////////
+//  Constants
+////////////////////////////////////////////////////////////////////////
+
+    private static final    int NUM_COLUMNS = 40;
+
+////////////////////////////////////////////////////////////////////////
+//  Constructors
+////////////////////////////////////////////////////////////////////////
+
+    public FPathnameField( )
+    {
+        super( NUM_COLUMNS );
+        _init( );
+    }
+
+    //------------------------------------------------------------------
+
+    public FPathnameField( String pathname )
+    {
+        super( pathname, NUM_COLUMNS );
+        _init( );
+    }
+
+    //------------------------------------------------------------------
+
+    public FPathnameField( File file )
+    {
+        super( file, NUM_COLUMNS );
+        _init( );
+    }
+
+    //------------------------------------------------------------------
+
+////////////////////////////////////////////////////////////////////////
+//  Class methods
+////////////////////////////////////////////////////////////////////////
+
+    public static void addObserver( String         key,
+                                    FPathnameField field )
+    {
+        List<FPathnameField> fields = observers.get( key );
+        if ( fields == null )
+        {
+            fields = new ArrayList<>( );
+            observers.put( key, fields );
+        }
+        fields.add( field );
+        AppConfig.getInstance( ).addShowUnixPathnamesObserver( field );
+    }
+
+    //------------------------------------------------------------------
+
+    public static void removeObservers( String key )
+    {
+        List<FPathnameField> fields = observers.get( key );
+        if ( fields != null )
+        {
+            for ( FPathnameField field : fields )
+                AppConfig.getInstance( ).removeShowUnixPathnamesObserver( field );
+            observers.remove( key );
+        }
+    }
+
+    //------------------------------------------------------------------
+
+////////////////////////////////////////////////////////////////////////
+//  Instance methods
+////////////////////////////////////////////////////////////////////////
+
+    private void _init( )
+    {
+        AppFont.TEXT_FIELD.apply( this );
+        GuiUtilities.setTextComponentMargins( this );
+        setUnixStyle( AppConfig.getInstance( ).isShowUnixPathnames( ) );
+    }
+
+    //------------------------------------------------------------------
+
+////////////////////////////////////////////////////////////////////////
+//  Class variables
+////////////////////////////////////////////////////////////////////////
+
+    private static  Map<String, List<FPathnameField>>   observers   = new HashMap<>( );
+
+}
+
+//----------------------------------------------------------------------
